249daaf0c8367fb01fd80ca1a18be51c7ee3bf7e
Matheus Cruz <64933816+suehtamCruz@users.noreply.github.com>
2020-10-18 19:19:37 -0300

    Create receita.ts

---
+++ controllers/receita.ts
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from 'mongoose';
+import Receitas from '../models/receitas';
+const receitaSchema = Receitas.model('Receitas');
+
+
+async function findReceita(){
+
+}   
+async function createReceita(){
+
+}   
+async function updateReceita(){
+
+}   
+async function deleteReceita(){
+
+}   
