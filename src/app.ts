4a73d68be4d37c246605c790284b1aec919f7085
Matheus Cruz <64933816+suehtamCruz@users.noreply.github.com>
2020-10-21 21:32:41 -0300

    cors added

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,14 @@ const app = express();
 import  mongoose from 'mongoose';
 
 import router from '../routes/routes';
+import cors from 'cors';
 
 const connection = mongoose.connect('mongodb+srv://matheus:matheus@copper.rfczh.mongodb.net/receitas_db?retryWrites=true&w=majority',{useNewUrlParser: true ,useUnifiedTopology: true });
 
 if(connection){
     console.log("API CONECTADA AO BANCO");
 }
+app.use(cors());
 
 app.listen(3003);
 app.use(express.json());
